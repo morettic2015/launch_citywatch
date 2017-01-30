73690bac62ed219957071366d39e3ee984313f63
SEO <malacma@gmail.com>
2017-01-30 17:44:35 -0200

    MVP

--- inc/help.php
+++ inc/help.php
@@ -10,10 +10,11 @@
     <div id="newsletterform" class="noSpace" >
         <div class="columnsContainer" >
             <div class="leftColumn">
-                <img src="./assets/images/help_01.svg" >
+                <img src="./assets/images/help_01.svg" height="400">
             </div>
             <div class="rightColumn">
-                <h4>ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a</h4>
+                <h2 style="margin-top: 70px">1º) Passo: Entrar ou cadastrar-se</h2>
+                <h4>Selecione sua rede social preferida, Facebook, Google ou Twitter e acesse sua conta ou cadastre-se</h4>
             </div>
         </div>
     </div>
@@ -25,12 +26,18 @@
     <div id="newsletterform" class="noSpace" >
         <div class="columnsContainer" >
             <div class="leftColumn">
-
-                <h4>ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a</h4>
-
+                <h2 class="whiteOne" style="margin-top: 70px">2º) Selecione umas das opções do APP</h2>
+                <h4 class="whiteOne">
+                    <ul style="text-align: left">
+                        <li>Procure experiências</li>
+                        <li>O que está acontecendo</li>
+                        <li>Compartilhar experiência</li>
+                    </ul>
+                </h4>
             </div>
             <div class="rightColumn">
-                <img src="./assets/images/help_02.svg" ></div>
+                <img src="./assets/images/help_02.svg"  height="400">
+            </div>
         </div>
     </div>
 </div>
@@ -41,10 +48,11 @@
     <div id="newsletterform" class="noSpace" >
         <div class="columnsContainer" >
             <div class="leftColumn">
-                <img src="./assets/images/help_03.svg" >
+                <img src="./assets/images/help_03.svg"  height="400">
             </div>
             <div class="rightColumn">
-                <h4>ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a</h4>
+                <h2 style="margin-top: 70px">3º) Pesquisar experiências</h2>
+                <h4>Selecione as categorias de seu interesse, informe o raio de pesquisa para visualizar as experiências no mapa ao seu redor.</h4>
             </div>
         </div>
     </div>
@@ -56,44 +64,11 @@
     <div id="newsletterform" class="noSpace" >
         <div class="columnsContainer" >
             <div class="leftColumn">
-
-                <h4>ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a</h4>
-
+                <h2 class="whiteOne" style="margin-top: 70px">4º) Visualizar Experiências</h2>
+                <h4 class="whiteOne">A partir dos resultados da pesquisa geolocalizada você pode ficar informado dos estabelecimento, eventos e experiências ao seu redor. Basta clicar no icone da experiência para saber mais.</h4>
             </div>
             <div class="rightColumn">
-                <img src="./assets/images/help_04.svg" ></div>
-        </div>
-    </div>
-</div>
-
-<div  class="barra_up">
-    <img src="./assets/images/Cinza_claro11.svg" style="width: 100%"/>
-</div>
-<div class="landindPage_carac">
-    <div id="newsletterform" class="noSpace" >
-        <div class="columnsContainer" >
-            <div class="leftColumn">
-                <img src="./assets/images/help_05.svg" >
-            </div>
-            <div class="rightColumn">
-                <h4>ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a</h4>
-            </div>
-        </div>
-    </div>
-</div>
-<div  class="barra_up">
-    <img src="./assets/images/Cinza_claro12.svg" style="width: 100%"/>
-</div>
-<div class="landindPage_who">
-    <div id="newsletterform" class="noSpace" >
-        <div class="columnsContainer" >
-            <div class="leftColumn">
-
-                <h4>ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a</h4>
-
-            </div>
-            <div class="rightColumn">
-                <img src="./assets/images/help_06.svg" ></div>
+                <img src="./assets/images/help_04.svg" height="400"></div>
         </div>
     </div>
 </div>
@@ -105,10 +80,11 @@
     <div id="newsletterform" class="noSpace" >
         <div class="columnsContainer" >
             <div class="leftColumn">
-                <img src="./assets/images/help_07.svg" >
+                <img src="./assets/images/help_06.svg" height="400">
             </div>
             <div class="rightColumn">
-                <h4>ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a</h4>
+                <h2 style="margin-top: 70px">5º) Registrar Experiência</h2>
+                <h4 >Cadastre sua experiência informando o local a descrição, categorias e anexando imagens. O App tem a opção de reconhecimento de voz para agilizar o cadastro da experiência</h4>
             </div>
         </div>
     </div>
@@ -120,12 +96,28 @@
     <div id="newsletterform" class="noSpace" >
         <div class="columnsContainer" >
             <div class="leftColumn">
-
-                <h4>ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a ASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf aASDASDASDASDA asdf asdf asdf asdfa sfdasdf asdf a</h4>
-
+                <h2 class="whiteOne" style="margin-top: 70px">6º) O que está acontecendo</h2>
+                <h4 class="whiteOne">Veja as últimas experências na sua região direto do Twitter, Google Places, Webhose no nosso Bigdata e muito mais. Experimente!</h4>
             </div>
             <div class="rightColumn">
-                <img src="./assets/images/help_08.svg" ></div>
+                <img src="./assets/images/help_08.svg" height="400"></div>
+        </div>
+    </div>
+</div>
+
+<div  class="barra_up">
+    <img src="./assets/images/Cinza_claro11.svg" style="width: 100%"/>
+</div>
+<div class="landindPage_carac">
+    <div id="newsletterform" class="noSpace" >
+        <div class="columnsContainer" >
+            <div class="leftColumn">
+                <img src="./assets/images/help_10.svg" height="400">
+            </div>
+            <div class="rightColumn">
+                <h2 style="margin-top: 70px">7º) Menu de navegação</h2>
+                <h4 >Através do menu de navegação você tem um atalho para todas as funções do APP. </h4>
+            </div>
         </div>
     </div>
 </div>
